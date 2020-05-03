1dddbbf4aa8a9e2fa21d128d35e72cba48605267
fusidic <arithbar@gmail.com>
2020-05-04 00:36:45 +0800

    feature: add jwt token auth

--- main.go
+++ main.go
@@ -1,89 +1,62 @@
 package main
 
 import (
+	"context"
 	"log"
 	"os"
 
 	pb "github.com/fusidic/user-service/proto/user"
-	"github.com/micro/cli"
 	"github.com/micro/go-micro"
 	microclient "github.com/micro/go-micro/client"
-	"github.com/micro/go-micro/config/cmd"
-	"golang.org/x/net/context"
+	"github.com/prometheus/common/log"
 )
 
 func main() {
+	srv := micro.NewService(
+		micro.Name("user-cli"),
+		micro.Version("latest"),
+	)
 
-	cmd.Init()
+	// 初始化并解析命令行参数
+	srv.Init()
 
-	// 创建 user-service 服务的客户端
 	client := pb.NewUserServiceClient("user", microclient.DefaultClient)
 
-	// 设置命令行调用的参数
-	service := micro.NewService(
-		micro.Flags(
-			cli.StringFlag{
-				Name:  "name",
-				Usage: "You full name",
-			},
-			cli.StringFlag{
-				Name:  "email",
-				Usage: "Your email",
-			},
-			cli.StringFlag{
-				Name:  "password",
-				Usage: "Your password",
-			},
-			cli.StringFlag{
-				Name:  "company",
-				Usage: "Your company",
-			},
-		),
-	)
+	name := "Ewan Valentine"
+	email := "ewan.valentine89@gmail.com"
+	password := "test123"
+	company := "BBC"
 
-	// Start as service
-	service.Init(
-
-		micro.Action(func(c *cli.Context) {
-
-			name := c.String("name")
-			email := c.String("email")
-			password := c.String("password")
-			company := c.String("company")
-
-			if name == "" {
-				name = "blank"
-				email = "test@blank"
-				password = "blank"
-				company = "BLANK"
-				log.Printf("unlucky :( name:%s email:%s password:%s company:%s", name, email, password, company)
-			}
-			// Call our user service
-			r, err := client.Create(context.TODO(), &pb.User{
-				Name:     name,
-				Email:    email,
-				Password: password,
-				Company:  company,
-			})
-			if err != nil {
-				log.Fatalf("Could not create: %v", err)
-			}
-			log.Printf("Created: %s", r.User.Id)
-
-			getAll, err := client.GetAll(context.Background(), &pb.Request{})
-			if err != nil {
-				log.Fatalf("Could not list users: %v", err)
-			}
-			for _, v := range getAll.Users {
-				log.Println(v)
-			}
-
-			os.Exit(0)
-		}),
-	)
-
-	// Run the server
-	if err := service.Run(); err != nil {
-		log.Println(err)
+	r, err := client.Create(context.TODO(), &pb.User{
+		Name:     name,
+		Email:    email,
+		Password: password,
+		Company:  company,
+	})
+	if err != nil {
+		log.Fatalf("Could not create: %v", err)
 	}
+	log.Printf("Created: %s", r.User.Id)
+
+	getAll, err := client.GetAll(context.Background(), &pb.Request{})
+	if err != nil {
+		log.Fatalf("Could not list users: %v", err)
+	}
+	for _, v := range getAll.Users {
+		log.Println(v)
+	}
+
+	authResponse, err := client.Auth(context.TODO(), &pb.User{
+		Email:    email,
+		Password: password,
+	})
+
+	if err != nil {
+		log.Fatalf("Could not authenticate user: %s error: %v \n", email, err)
+	}
+
+	log.Printf("Your access token is: %s \n", authResponse.Token)
+
+	// 退出
+	os.Exit(0)
 }
